fc017cb9247c390060caf6c599dfa88ddab43e8e
0ssigeno <simone.berni2@studio.unibo.it>
2022-06-28 17:23:18 +0200

    Fix path + black

--- test/python_test/python_test/asgi.py
+++ test/python_test/python_test/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_test.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_test.settings")
 
 application = get_asgi_application()
--- test/python_test/python_test/wsgi.py
+++ test/python_test/python_test/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_test.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_test.settings")
 
 application = get_wsgi_application()
