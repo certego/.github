2626b590cd169b34dd12abf73fd3732c376ecad1
0ssigeno <simone.berni2@studio.unibo.it>
2022-07-07 15:39:58 +0200

    Pytest_marker_for_master

--- test/python_test/test_source.py
+++ test/python_test/test_source.py
@@ -1,12 +1,17 @@
 import unittest
+import pytest
 
 try:
     from .source import A
 except ImportError:
     from source import A
 
-
 class TestA(unittest.TestCase):
     def test_mya(self):
+        a = A()
+        self.assertEqual(a.my_a(), 1)
+
+    @pytest.mark.master
+    def test_mya2(self):
         a = A()
         self.assertEqual(a.my_a(), 1)
