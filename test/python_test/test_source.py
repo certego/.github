467d649082944018b44f87dd9c8028cd73972ef5
0ssigeno <simone.berni2@studio.unibo.it>
2022-07-12 11:47:27 +0200

    Added test

--- test/python_test/test_source.py
+++ test/python_test/test_source.py
@@ -10,3 +10,10 @@ class TestA(unittest.TestCase):
     def test_mya(self):
         a = A()
         self.assertEqual(a.my_a(), 1)
+
+class TestB(unittest.TestCase):
+
+    def test_add_task(self):
+        from python_test.celery import add
+        rst = add.apply(args=(4, 4)).get()
+        self.assertEqual(rst, 8)
