d810f1fc86d7c7ca5a561fdb67f858282a11b9ff
Simone Berni <simone.berni2@studio.unibo.it>
2022-07-26 12:00:25 +0200

    Allow the usage of slow tags to have tests only run in master/main (#86)
    * boilerplate
    
    * Fixed description + added input on workflows + added test
    
    * Black
    
    * Added tag_for_manual_tests
    
    * flake
    
    * Typo

--- test/python_test/test_source.py
+++ test/python_test/test_source.py
@@ -1,5 +1,7 @@
 import unittest
 
+from django.test import tag
+
 try:
     from .source import A
 except ImportError:
@@ -10,6 +12,16 @@ class TestA(unittest.TestCase):
     def test_mya(self):
         a = A()
         self.assertEqual(a.my_a(), 1)
+
+    @tag("main")
+    def test_c(self):
+        a = A()
+        self.assertEqual(a.c, 3)
+
+    @tag("manual")
+    def test_b(self):
+        a = A()
+        self.assertEqual(a.b, 2)
 
 
 class TestB(unittest.TestCase):
